bd1923adee619ad59e6dcf7b9a0794abe3c6c666
Aaron Dewes <aaron.dewes@web.de>
2021-12-03 19:34:35 +0000

    Some improvements and start working the Vision 6

---
+++ README.md
@@ -0,0 +1,19 @@
+# Tolino hacking
+
+A tool to hack Tolino ebook readers.
+
+**THIS DOES NOT WORK FOR THE VISION 6**
+
+---
+
+This tool allows you to enable adb, root access and the developer options on your Tolino.
+
+### Included scripts
+
+`boot_adb_mode.sh`: This boots your Tolino into a rooted mode with adb enabled. From this mode, you can, until a reboot, apply modifications to the OS. The modifications you apply will be kept, adb and root access won't.
+`enable_dev_options.sh` This will permanently enable the developer options on your Tolino. This only works while the Tolino is in adb mode (via `boot_adb_mode.sh`). To access the developer options, press the search icon on the tolino homepage.
+
+### Other things
+
+`mkbootimg_V6.py` has a hardcoded OS version and patch level because I couldn't figure out how these are generated (yet).
+
---
+++ wip_vision6.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+# cd into the dir which contains this file
+cd "$(dirname "${BASH_SOURCE}")"
+
+# Download https://download.pageplace.de/ereader/15.2.0/OS81/update.zip to work-v6/update.zip
+# Unzip work-v6/update.zip to work-v6/update
+# If update.ip doesn't already exist in work-v6
+mkdir -p work-v6
+echo "Downloading update..."
+if [ ! -f work-v6/update.zip ]; then
+  # Download https://download.pageplace.de/ereader/15.2.0/OS81/update.zip to work-v6/update.zip
+  curl -L -o work-v6/update.zip https://download.pageplace.de/ereader/15.2.0/OS81/update.zip
+fi
+
+rm -rf work-v6/update work-v6/boot-15.2.0
+
+# Extract only the boot.img from the update.zip
+echo "Extracting boot.img from update.zip..."
+unzip -j work-v6/update.zip boot.img -d work-v6/update
+echo "Extracting ramdisk from boot.img..."
+./unpack_bootimg.py --boot_img work-v6/update/boot.img --out work-v6/boot-15.2.0 
+echo "Unpacking ramdisk..."
+mkdir work-v6/boot-15.2.0/ramdisk-unpacked
+cd work-v6/boot-15.2.0/ramdisk-unpacked
+gunzip -c ../ramdisk | cpio -i
+
+rm default.prop
+echo "Hacking ramdisk to enable adb..."
+# Put new, multiline content into default.prop
+cat ../../../patched/default-v6.prop > default.prop
+
+echo "Repacking ramdisk..."
+find . | cpio -o -H newc | gzip > ../repack-ramdisk
+cd ..
+rm -rf ramdisk-unpacked
+rm -rf ramdisk
+mv repack-ramdisk ramdisk
+
+echo "Creating new boot.img..."
+cd ..
+cd ..
+./mkbootimg_V6.py --kernel work-v6/boot-15.2.0/kernel --ramdisk work-v6/boot-15.2.0/ramdisk \
+  --cmdline "selinux=1 androidboot.selinux=permissive buildvariant=user"\
+  --kernel_offset 0x30008000 --ramdisk_offset 0x32000000\
+  --second_offset 0x30f00000 --tags_offset 0x30000100\
+  --os_patch_level 268697895\
+  -o work-v6/boot-15.2.0-patched.img
+exit 1
+# Now, wait for the user to press enter
+echo
+echo "Preparations done!"
+echo "Please now boot your tolino into recovery mode by holding the power button until the screen refreshes and shows the Tolino logo."
+echo "This can take up to 1 minute."
+fastboot boot work-v6/boot-15.2.0-patched.img
